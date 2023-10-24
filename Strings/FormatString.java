7ade12bc63fe7c656e5f2d3aadf498c7e022c5a8
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-10-24 18:13:21 -0400

    added a line to FormatString.java

--- Strings/FormatString.java
+++ Strings/FormatString.java
@@ -10,7 +10,8 @@ public class FormatString {
             System.out.print("Enter a number: ");
             int number = scanner.nextInt();
             System.out.println();
-            System.out.format("The word you entered is \"%s\" and the number you entered is %d.", input, number);
+            System.out.format("The word you entered is \"%s\" and the number you entered is %d.\n", input, number);
+            System.out.format("The text that you have entered has a length of %d.\n", input.length());
         }
         
     }
