78cd70a4ee3c6de3484b21ec8507614896569ca1
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2023-11-02 17:09:37 -0400

    added GetterAndSetters and made getter/setters using SourceAction

---
+++ SwitchItUp/GettersAndSetters.java
@@ -0,0 +1,44 @@
+package SwitchItUp;
+
+public class GettersAndSetters {
+    String firstName;
+    String lastName;
+    int age;
+    String email;
+    
+    public GettersAndSetters() {
+    }
+    
+    public GettersAndSetters(String firstName, String lastName, int age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
